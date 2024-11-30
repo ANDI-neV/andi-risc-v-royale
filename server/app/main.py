7bb6bb671c44a0e7fa50ac499bc94e8483306a96
Leon Bartle <leon@bartle.eu>
2024-11-30 13:01:10 +0100

    Functional test python server

--- server/app/main.py
+++ server/app/main.py
@@ -5,10 +5,12 @@ app = Flask(__name__)
 @app.route('/activity', methods=['GET', 'POST'])
 def reply():
     if request.method == 'GET':
+        print(request.args)
         return jsonify({"message": "Hello from GET!"})
     elif request.method == 'POST':
         data = request.json
+        print(data)
         return jsonify({"message": "Received from POST", "data": data})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=11449, host="0.0.0.0")
